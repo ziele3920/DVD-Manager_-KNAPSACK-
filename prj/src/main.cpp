39cf000776f825b646010ea5faeccb9c2975f2a4
ziele3920 <209296@student.pwr.edu.pl>
2015-06-01 14:34:09 +0200

    stale jednostek w mainie

--- prj/src/main.cpp
+++ prj/src/main.cpp
@@ -1,5 +1,10 @@
 #include "../inc/DataGen.hh"
 
+// Jedostka podstawowa KB !!
+const int MB = 1000;
+const int GB = 1000000;
+const int TB = 1000000000;
+
 int main() {
   std::string fileName = "dane.dat";
   DataGen DG;
